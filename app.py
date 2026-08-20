0dce8c45643959b8af0b90bc3f3cee863787c705
Tamertotos <tamercantotos@gmail.com>
2026-08-20 17:42:29 +0300

    added timer logic, and moved image to a dir.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter
+import datetime as dt
 
 FONT = ("Courier",20,"bold")
 LATITUDE = 52.237049
@@ -9,7 +10,7 @@ class App(tkinter.Tk):
         super().__init__()
         self.title("ISS Locator")
         self.minsize(1280,720)
-        self.image = tkinter.PhotoImage(file="world_map.png")
+        self.image = tkinter.PhotoImage(file="Images/world_map.png")
 
         self.build_canvas()
         self.build_labels()
@@ -20,9 +21,20 @@ class App(tkinter.Tk):
         self.canvas.place(x=0,y=100)
 
     def build_labels(self):
-        self.label1 = tkinter.Label(self,text= "Current Timee\n123", font=FONT)
-        self.label1.place(x=900,y=0)
+        self.label1 = tkinter.Label(self,text="", font=FONT)
+        self.label1.place(x=900,y=20)
 
         self.label2 = tkinter.Label(self,text=f"Location\nLatitude = {LATITUDE}\nLongitude = {LONGITUDE}" , font=FONT)
         self.label2.place(x=500,y=0)
-    
+
+    def change_text(self):
+        self.label1.config(text=f"Current time\n{dt.datetime.now().strftime("%d.%m.%Y %H:%M")}")
+
+class Logic:
+    def __init__(self, app):
+        self.gui_app = app
+        self.timer()
+
+    def timer(self):
+        self.gui_app.change_text()
+        self.gui_app.after(60000,self.timer)
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from app import App
+from app import App, Logic
 
 
 def main():
     world_app = App()
-
+    world_app_logic = Logic(world_app)
 
     world_app.mainloop()
 
